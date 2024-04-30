1f474c113be237f1b3f740ff74b1bbe710469aa0
Ayokunnumi1 <omololuayk@gmail.com>
2024-04-30 22:07:05 +0100

    Add tailwind extensions for image & background colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-gray": "#2A3132",
+      },
+      backgroundImage: (theme) => ({
+        "home-pg-bg-image": "url('../src/asset/tech-bg.jpg')",
+      }),
+    },
   },
   plugins: [],
-}
-
+};
