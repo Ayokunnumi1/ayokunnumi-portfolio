591f6474fc314815cb38d4d11232e011a8320d6e
Ayokunnumi1 <omololuayk@gmail.com>
2024-05-06 23:03:22 +0100

    Import react link smooth scroll

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "../src/app.css";
 import Home from "./component/Home";
+import About from "./component/About";
+import Portfolio from "./component/Portfolio";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <AnchorLink id="#About">
+        <About />
+      </AnchorLink>
+      <AnchorLink id="#Portfolio">
+        <Portfolio />
+      </AnchorLink>
     </div>
   );
 }
