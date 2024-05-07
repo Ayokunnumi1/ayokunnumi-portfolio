cb3cc6e7ccd97529049fd0a4b7cec48c7576a577
Ayokunnumi1 <omololuayk@gmail.com>
2024-05-07 17:07:24 +0100

    Import all components in app file

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import "../src/app.css";
 import Home from "./component/Home";
 import About from "./component/About";
 import Portfolio from "./component/Portfolio";
-import AnchorLink from "react-anchor-link-smooth-scroll";
+import MyServices from "./component/MyServices";
+import Testimonials from "./component/Testimonials";
+import MySkills from "./component/MySkills";
+import InterestingFacts from "./component/InterestingFacts";
+import MyContent from "./component/MyContent";
+import Contact from "./component/Contact";
+// import AnchorLink from "react-anchor-link-smooth-scroll";
 
 function App() {
   return (
     <div className="App">
       <Home />
-      <AnchorLink id="#About">
-        <About />
-      </AnchorLink>
-      <AnchorLink id="#Portfolio">
-        <Portfolio />
-      </AnchorLink>
+      <MyServices />
+      <About />
+      <Portfolio />
+      <Testimonials />
+      <MySkills />
+      <InterestingFacts />
+      <MyContent />
+      <Contact />
     </div>
   );
 }
