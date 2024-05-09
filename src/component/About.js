b9b8d5c27af9cd252ab3f4cb5443db0c31bba606
Ayokunnumi1 <omololuayk@gmail.com>
2024-05-09 20:23:44 +0100

    Adjust background image for all screen sizes

--- src/component/About.js
+++ src/component/About.js
@@ -2,7 +2,7 @@ import "../styles/about.css"
 
 const About = () => {
     return (
-      <section className="bg-home-pg-bg-image bg-no-repeat bg-cover text-white p-10">
+      <section className="bg-home-pg-bg-image bg-no-repeat bg-cover text-white w-screen sm:h-506 md:h-96 bg-center bg-[100% auto] py-10">
         <div className="relative z-10 flex flex-col justify-center items-center gap-5">
           <div className="flex flex-col items-center">
             <h3 className="about_me_heading">About Me</h3>
