017b44f418a907e5040a7d9add7c46b8186c1990
Ayokunnumi1 <omololuayk@gmail.com>
2024-05-16 23:21:59 +0100

    Use custom gap for xtra large screen size

--- src/component/InterestingFacts.js
+++ src/component/InterestingFacts.js
@@ -10,7 +10,7 @@ const InterestingFacts = () => {
           <div className="w-20 h-1 bg-custom-green"></div>
         </div>
 
-        <article className="text-white flex flex-col sd:flex-row sd:justify-center sd:items-center gap-4 sd:gap-16 md:gap-48 lg:gap-80 sd:mt-8">
+        <article className="text-white flex flex-col sd:flex-row sd:justify-center sd:items-center gap-4 sd:gap-16 md:gap-48 lg:gap-80 xlg:gap-gap-1 sd:mt-8">
           <div className="flex flex-col gap-6 sd:gap-10">
             <div className="flex gap-4">
               <div className="w-14 h-14 lg:w-20 lg:h-20 rounded-full border-2 border-custom-green flex flex-col justify-center items-center">
