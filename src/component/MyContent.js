2b95da9d068bd484936dcb9a2964918f51820f04
Ayokunnumi1 <omololuayk@gmail.com>
2024-05-16 23:35:53 +0100

    Put my blog contents in an article tag

--- src/component/MyContent.js
+++ src/component/MyContent.js
@@ -11,20 +11,60 @@ const MyContent = () => {
           <span className="text-custom-green">Content</span>
         </h3>
         <h3 className="what-i-use mx-5 mt-0">What I Share</h3>
-        <div className="rounded-3xl border-2 border-custom-green mx-10">
-          <img
-            className="bg-cover bg-no-repeat rounded-3xl w-32 h-10"
-            src={ReactContent}
-            alt="react course"
-          />
-          <p className="text-center">
-            <span>React</span>
-            <span> Oct 8th, 2023</span>
-          </p>
-          <p className="text-center">
-            Create your dream space hub with React & Redux
-          </p>
-        </div>
+        <article className="flex flex-col gap-10 md:flex-row mx-10 mt-8">
+          <div className="rounded-3xl border-2 border-custom-green ">
+            <img
+              className="bg-cover bg-no-repeat w-full rounded-3xl h-56"
+              src={ReactContent}
+              alt="react content"
+            />
+            <div className="p-5">
+              <p className="text-center mb-3">
+                <span className="text-custom-green content-text">React</span>
+                <span className="content-date"> Oct 8th, 2023</span>
+              </p>
+              <p className="text-center content-text">
+                Create your dream space hub with React & Redux
+              </p>
+            </div>
+          </div>
+
+          <div className="rounded-3xl border-2 border-custom-green">
+            <img
+              className="bg-cover bg-no-repeat w-full rounded-3xl h-56"
+              src={RubyContent}
+              alt="ruby content"
+            />
+            <div className="p-5">
+              <p className="text-center mb-3">
+                <span className="text-custom-green content-text">Ruby</span>
+                <span className="content-date"> March 7th, 2024</span>
+              </p>
+              <p className="text-center content-text">
+                Build your first Ruby Command Line Interface in 1hr
+              </p>
+            </div>
+          </div>
+
+          <div className="rounded-3xl border-2 border-custom-green">
+            <img
+              className="bg-cover bg-no-repeat w-full rounded-3xl h-56"
+              src={transitionContent}
+              alt="my transition to tech"
+            />
+            <div className="p-5">
+              <p className="text-center mb-3">
+                <span className="text-custom-green content-text">
+                  Transition
+                </span>
+                <span className="content-date"> Jan 6th, 2024</span>
+              </p>
+              <p className="text-center content-text">
+                I struggled a lot with having a career change.
+              </p>
+            </div>
+          </div>
+        </article>
       </div>
     </section>
   );
