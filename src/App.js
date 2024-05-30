6e94da65caf20fa728736bd1d6f1e7759da12a23
Ayokunnumi1 <omololuayk@gmail.com>
2024-05-30 21:11:00 +0100

    Use browser router for navigation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "../src/app.css";
+import { BrowserRouter } from "react-router-dom";
 import Home from "./component/Home";
 import About from "./component/About";
 import Portfolio from "./component/Portfolio";
@@ -8,20 +9,21 @@ import MySkills from "./component/MySkills";
 import InterestingFacts from "./component/InterestingFacts";
 import MyContent from "./component/MyContent";
 import Contact from "./component/Contact";
-// import AnchorLink from "react-anchor-link-smooth-scroll";
 
 function App() {
   return (
     <div className="App overlayy">
-      <Home />
-      <MyServices />
-      <About />
-      <Portfolio />
-      <Testimonials />
-      <MySkills />
-      <InterestingFacts />
-      <MyContent />
-      <Contact />
+      <BrowserRouter>
+        <Home />
+        <MyServices />
+        <About />
+        <Portfolio />
+        <Testimonials />
+        <MySkills />
+        <InterestingFacts />
+        <MyContent />
+        <Contact />
+      </BrowserRouter>
     </div>
   );
 }
