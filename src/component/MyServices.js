b60755ba9fc70d516dbe5209bb9a1cbe968023a9
Ayokunnumi1 <omololuayk@gmail.com>
2024-06-04 21:00:21 +0100

    Include css transform styling

--- src/component/MyServices.js
+++ src/component/MyServices.js
@@ -1,4 +1,4 @@
-import useVisibilityObserver from '../hooks/useVisibilityObserver';
+import useVisibilityObserver from "../hooks/useVisibilityObserver";
 import FrontendImg from "../assets/images/frontend.svg";
 import BackendImg from "../assets/images/backend.svg";
 import FullStackImg from "../assets/images/fullStack.svg";
@@ -7,17 +7,19 @@ import "../app.css";
 const MyServices = () => {
   const [addToRef] = useVisibilityObserver();
 
-
   return (
     <section className="bg-home-pg-bg-image bg-no-repeat bg-cover text-white pb-10">
-      <div className="relative z-10 md:px-10 lg:px-20">
+      <div className="relative z-10 lg:px-20">
         <h3 className="my-services px-5 pt-5">
           <span>My </span>
           <span className="text-custom-green">Services</span>
         </h3>
         <h3 className="what-i-do mx-5 mt-0">What I Do</h3>
-        <article className="sm:flex flex-col justify-center items-center md:flex-row text-center">
-          <div ref={addToRef} className="bg-custom-black border-radius flex flex-col justify-center items-center p-10 m-5 md:p-2 container">
+        <article className="flex flex-col justify-center items-center md:flex-row text-center mx-10">
+          <div
+            ref={addToRef}
+            className="zoom-out bg-custom-black border-radius flex flex-col justify-center items-center p-10 m-5 md:p-2 container"
+          >
             <img src={FrontendImg} alt="frontend" className="myServiceImg" />
             <h4 className="my-service-title md:text-nowrap">
               Front End Developer
@@ -31,7 +33,10 @@ const MyServices = () => {
               Microverse students build React project
             </p>
           </div>
-          <div ref={addToRef} className="bg-custom-black border-radius flex flex-col justify-center items-center p-10 m-5 md:p-2 container">
+          <div
+            ref={addToRef}
+            className="zoom-out bg-custom-black border-radius flex flex-col justify-center items-center p-10 m-5 md:p-2 container"
+          >
             <img src={BackendImg} alt="Backend" className="myServiceImg" />
             <h4 className="my-service-title md:text-nowrap">
               Back End Developer
@@ -42,7 +47,10 @@ const MyServices = () => {
               build Command Line Interface
             </p>
           </div>
-          <div ref={addToRef} className="bg-custom-black border-radius flex flex-col justify-center items-center p-10 m-5 md:p-2 container">
+          <div
+            ref={addToRef}
+            className="zoom-out bg-custom-black border-radius flex flex-col justify-center items-center p-10 m-5 md:p-2 container"
+          >
             <img src={FullStackImg} alt="full stack" className="myServiceImg" />
             <h4 className="my-service-title md:text-nowrap">
               Full Stack Developer
