0968e71b18a56f978ae545c6103530f31505e391
Ayokunnumi1 <omololuayk@gmail.com>
2024-06-09 20:28:52 +0100

    Add new key for sanity client.js

--- src/client.js
+++ src/client.js
@@ -1,6 +1,7 @@
 import sanityClient from '@sanity/client'
 
 export default sanityClient({
-    projectId: "id7zsgea",
-    dataset: "production"
-})
+  projectId: "id7zsgea",
+  dataset: "production",
+  useCdn: true
+});
