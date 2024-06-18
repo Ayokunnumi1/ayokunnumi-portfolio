f1a9a8c1d0c921ef16ffd6079314e1dccd680211
Ayokunnumi1 <omololuayk@gmail.com>
2024-06-18 02:05:17 +0100

    Add a .env file to keep token

--- front-end/src/client.js
+++ front-end/src/client.js
@@ -5,9 +5,8 @@ const projectId = "id7zsgea";
 const dataset = "production";
 const apiVersion = "2022-02-01";
 const useCdn = true;
-const token =
-  "skxfm9Pmy3mwTMiBNWQqTx4V14yYlHv4Qe5KyUc8pdMEtimlKv0D1lTGFSXpzsXRdaVWx7MkR5H39wKlKBYLhnXU1qBVQVRoSYOoUDm3D6lHOsCynswQQye2UaB5SdEYip7x1TUVMq2KQXrWtPqwZDFUBaYxyvMIk757T2Z3q0IYc6vCjTfr";
-
+const token = process.env.REACT_APP_SANITY_TOKEN;
+  
 if (!projectId || !token) {
   throw new Error("Missing required environment variables");
 }
