666c6b19853cb8dbf511c55944f98b01453dbc0f
Ayokunnumi1 <omololuayk@gmail.com>
2025-03-13 11:05:37 +0100

    Update the about me section

--- front-end/src/component/About.js
+++ front-end/src/component/About.js
@@ -1,20 +1,24 @@
 import "../app.css";
 
 const About = () => {
-    return (
-      <section id='about' className="bg-home-pg-bg-image bg-no-repeat bg-cover w-full sm:h-506 md:h-80 bg-center bg-[100% auto] py-10">
-        <div className="relative z-10 flex flex-col justify-center items-center gap-5">
-          <div className="flex flex-col items-center">
-            <h3 className="about-me-heading">About Me</h3>
-            <div className="w-20 h-1 bg-custom-green"></div>
-          </div>
-          <p className="about-me-text lg:px-36 p-6">
-            "I specialize in building user centric applications for companies
-            with a vision for the future."
-          </p>
+  return (
+    <section
+      id="about"
+      className="bg-home-pg-bg-image bg-no-repeat bg-cover w-full sm:h-506 md:h-80 bg-center bg-[100% auto] py-10"
+    >
+      <div className="relative z-10 flex flex-col justify-center items-center gap-5">
+        <div className="flex flex-col items-center">
+          <h3 className="about-me-heading">About Me</h3>
+          <div className="w-20 h-1 bg-custom-green"></div>
         </div>
-      </section>
-    );
-}
+        <p className="about-me-text lg:px-36 p-6">
+          "Hackathon-winning Full-Stack Developer passionate about building
+          scalable, user-centric solutions and empowering others through
+          mentorship and innovative tech."
+        </p>
+      </div>
+    </section>
+  );
+};
 
 export default About;
